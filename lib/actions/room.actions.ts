a147660e2a8698ae0cb0a383753af17ceeccec00
AkhilSourav <akhil.s18@iiits.in>
2025-01-15 14:06:13 +0530

    Fifth Commit - Active Collaborating with different account

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -22,12 +22,30 @@ export const createDocument = async ({userId, email}: CreateDocumentParams) => {
         const room = await liveblocks.createRoom(roomId, {
             metadata,
             usersAccesses,
-            defaultAccesses: []
+            defaultAccesses: ["room:write"]
         });
         revalidatePath(`/`);
         return parseStringify(room);
 
     } catch (error){
         console.log(`Error happended while creating a room: ${error}`);
+    }
+}
+
+export const getDocument = async ({roomId, userId} : {roomId: string, userId: string}) => {
+    try {
+        const room = await liveblocks.getRoom(roomId);
+
+        // TODO: Bring this back when we have permission
+        
+        // const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+
+        // if (!hasAccess) {
+        //     throw new Error(`You don't have access to this Document`);
+        // }
+
+        return parseStringify(room);
+    } catch (error) {
+        console.log(`Error happended while fetching a room: ${error}`);
     }
 }
