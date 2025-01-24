876cb5c07e5c21ee56208805a5fbb2a153cd05c4
AkhilSourav <akhil.s18@iiits.in>
2025-01-23 23:31:14 -0600

    Eight Commit - Added mentions feature

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -2,16 +2,29 @@
 import { ClientSideSuspense, LiveblocksProvider } from '@liveblocks/react/suspense';
 import { ReactNode } from 'react';
 import Loader from '../components/Loader';
-import { getClerkUsers } from '@/lib/actions/user.actions';
+import { getClerkUsers, getDocumentUsers } from '@/lib/actions/user.actions';
+import { useUser } from '@clerk/nextjs';
 
 const Provider = ({children}: {children: ReactNode}) => {
+  const { user: clerkUser } = useUser();
+
   return (
     <LiveblocksProvider 
       authEndpoint="/api/liveblocks-auth"
       resolveUsers={async ({userIds}) => {
         const users = await getClerkUsers({userIds});
         return users;
-      }}>
+      }}
+      resolveMentionSuggestions={async ({ text, roomId }) => {
+        const roomUsers = await getDocumentUsers({
+          roomId: roomId, 
+          currentUser: clerkUser?.emailAddresses[0].emailAddress!, 
+        text: text
+      })
+      return roomUsers
+    }
+    }
+      >
         <ClientSideSuspense fallback={<Loader/ >}>
           {children}
         </ClientSideSuspense>
