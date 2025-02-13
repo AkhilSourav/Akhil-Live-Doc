211c02c4da2d3a99b2a0eef8921890cb2787f1e6
AkhilSourav <akhil.s18@iiits.in>
2025-02-13 11:20:20 -0600

    Adding Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ClerkProvider } from "@clerk/nextjs"
 import { dark } from "@clerk/themes"
 import Provider from "./Provider"
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -38,6 +39,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <Provider>
               {children}
               <Analytics />
+              <SpeedInsights />
             </Provider>
           </body>
         </html>
